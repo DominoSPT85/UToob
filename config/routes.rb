6d8365e17e8915399d4a331002d76001206dd77f
Shandra <shandradev85@gmail.com>
2019-03-09 09:58:40 -0700

    added controllers"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/show'
+  get 'comments/new'
+  get 'movies/index'
+  get 'movies/show'
+  get 'movies/new'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
