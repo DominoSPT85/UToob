d405b23bf2288cd47770488b08127a594f43f4ec
Shandra <shandradev85@gmail.com>
2019-03-09 10:08:17 -0700

    changed column type for duration

---
+++ db/migrate/20190309170610_change_duration_type.rb
@@ -0,0 +1,5 @@
+class ChangeDurationType < ActiveRecord::Migration[5.2]
+  def change
+    change_column :movies, :duration, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_165513) do
+ActiveRecord::Schema.define(version: 2019_03_09_170610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2019_03_09_165513) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.float "duration"
+    t.string "duration"
     t.string "genre"
     t.text "description"
     t.string "trailer"
