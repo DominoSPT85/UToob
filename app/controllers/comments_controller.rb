480d10b89db888b9737601347e73848c3bc89ac3
Elizabeth King <elizking.dev@gmail.com>
2019-03-09 10:36:47 -0700

    filled out movie & comment controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,50 @@
 class CommentsController < ApplicationController
+
+  before_action :find_comment, only: [:show, :edit, :update, :destroy]
+
   def index
+    @comments = Comment.all
   end
 
   def show
   end
 
   def new
+    @comment = current_user.comment.new(comment_params)
+  end
+
+  def create
+    @comment = current_user.comment.new(comment_params)
+    if @comment.save
+      redirect_to movies_show_path
+    else 
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @comment.update(comment.params)
+      redirect_to movies_show_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to movies_show_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def find_comment
+    @comment = Comment.find(params[:id]) 
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,48 @@
 class MoviesController < ApplicationController
+
+  before_action :find_movie, only: [:show, :edit, :update, :destroy]
+
   def index
+    @movie = Movie.all
   end
 
   def show
   end
 
   def new
+    @movie = Movie.new(movie_params)
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_index_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movies_index_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to  movies_index_path
+  end
+
+  private
+  def movie_params
+    params.require(:person).permit(:title, :duration, :genre, :description, :trailer)
+
+  def find_movie
+    @movie = Movie.find(params[:id])
   end
 end
